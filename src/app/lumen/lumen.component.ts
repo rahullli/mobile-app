c306fe90a07ae6c1f9a1dc45ce76a7355e0da0b8
Rahul <khandelwalr213@gmail.com>
2023-08-09 10:50:08 +0530

    Initial Setup

---
+++ src/app/lumen/lumen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lumen',
+  templateUrl: './lumen.component.html',
+  styleUrls: ['./lumen.component.scss']
+})
+export class LumenComponent {
+
+}
